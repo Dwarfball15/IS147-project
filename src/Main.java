3286e72a15a37c6f69628113d97e585eaf853811
Wdedeke <wdedeke1@umbc.edu>
2025-11-17 15:52:22 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main{
-    public static void main(String[] args){
+/**
+ * The Main class serves as the entry point for the IS147 Workout Tracker project.
+ * <p>
+ * This class will run the program once the team integrates all components 
+ * such as the DateTracker, User input, and validation modules.
+ * </p>
+ *
+ * @author Wuraola
+ * @version 1.0
+ */
+public class Main {
+
+    /**
+     * The main method used to launch the Workout Tracker application.
+     *
+     * @param args Command-line arguments (not used).
+     */
+    public static void main(String[] args) {
 
     }
 }
